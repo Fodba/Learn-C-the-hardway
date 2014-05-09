4899b5b4a62756e870287411cf72df99f1abf2cd
fef <ffodba@gmail.com>
2014-05-09 23:01:39 +0200

    Exercise 3: Formatted Printing

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 FLAGS=-Wall -g
 
 
-all: ex1
+all: ex1 ex3
 	make ex1
+	make ex3
 
 clean:
 	rm -f ex1
+	rm -f ex3
 
 
---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age=10;
+    double height=7200000;
+
+    printf("I am %.05d years old.\n",age);
+    printf("I am %'f inches tall.\n",height);
+
+    return 0;
+}
