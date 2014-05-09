9ad4188c2e5eff1f15bb545e523ec0ea0335d167
fef <ffodba@gmail.com>
2014-05-09 21:27:35 +0200

    Exercise 1: Dust Off That Compiler

---
+++ ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("This is the second text.");
+    puts("Another one");
+    puts("Finally the last.");
+    return 0;
+}
